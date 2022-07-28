f4954ec6da09b76459f1e36fde234e9c8588ca02
kanghyonghyun <hmoon826@gmail.com>
2022-07-29 00:55:14 +0900

    [stack] 괄호문자제거

---
+++ src/main/java/stack/StringBracket.java
@@ -0,0 +1,33 @@
+package stack;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class StringBracket {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        StringBracket stringBracket = new StringBracket();
+        System.out.println(stringBracket.printString(sc.next()));
+    }
+
+    private String printString(String value) {
+        String result = "";
+        Stack<Character> strings = new Stack<>();
+        for (char s : value.toCharArray()) {
+            if (s == '(') {
+                strings.push(s);
+            } else if (s == ')'){
+                if (!strings.isEmpty()) {
+                    strings.pop();
+                }
+            } else {
+                if (strings.isEmpty()) {
+                    result += s;
+                }
+            }
+        }
+        return result;
+    }
+}
