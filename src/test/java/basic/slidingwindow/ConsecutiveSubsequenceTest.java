b7e5b629fcf2f0375d951e44f02ec3db0db86bc9
kanghyonghyun <hmoon826@gmail.com>
2022-10-11 15:39:57 +0900

    [basic] sliding window : 연속부분수열

--- src/test/java/basic/slidingwindow/ConsecutiveSubsequenceTest.java
+++ src/test/java/basic/slidingwindow/ConsecutiveSubsequenceTest.java
@@ -1,10 +1,8 @@
 package basic.slidingwindow;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class ConsecutiveSubsequenceTest {
 
