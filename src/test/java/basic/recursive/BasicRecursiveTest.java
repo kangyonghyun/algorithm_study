a35a0c602c19e94f4cd4fc12799ccc4ca4a7b428
kanghyonghyun <hmoon826@gmail.com>
2022-10-13 18:06:54 +0900

    [basic] recursive : 재귀함수로 1-N 까지 출력

--- src/test/java/basic/recursive/BasicRecursiveTest.java
+++ src/test/java/basic/recursive/BasicRecursiveTest.java
@@ -14,8 +14,12 @@ class BasicRecursiveTest {
     @ValueSource(ints = 3)
     void recursive(int n) {
         assertThat(solution(n)).containsExactly(1, 2, 3);
+        assertThat(solution1(n)).containsExactly(1, 2, 3);
     }
 
+    /**
+     * 재귀함수를 돌려서 List 객체에 담기
+     */
     public List<Integer> solution(int n) {
         List<Integer> results = new ArrayList<>();
         recursiveFunction(n, results);
@@ -23,9 +27,34 @@ class BasicRecursiveTest {
     }
 
     private void recursiveFunction(int n, List<Integer> result) {
-        if (n > 0) {
-            recursiveFunction(n - 1, result);
-            result.add(n);
+        if (n == 0) {
+            return;
+        }
+        recursiveFunction(n - 1, result);
+        result.add(n);
+    }
+
+    /**
+     * 객체 생성해서 배열에 담기
+     */
+    public int[] solution1(int n) {
+        Recursive recursive = new Recursive(n);
+        recursive.getAllNumbersWithRec(n);
+
+        return recursive.numbers;
+    }
+
+    static class Recursive {
+        int[] numbers;
+        public Recursive(int n) {
+            this.numbers = new int[n];
+        }
+        public void getAllNumbersWithRec(int n) {
+            if (n == 0) {
+                return;
+            }
+            getAllNumbersWithRec(n - 1);
+            numbers[n - 1] = n;
         }
     }
 
