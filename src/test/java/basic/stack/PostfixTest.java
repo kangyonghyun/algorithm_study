0e855c308729ec9007468caaf86992d27dc974d7
kanghyonghyun <hmoon826@gmail.com>
2022-11-22 19:47:49 +0900

    [refactor] Stack : 후위연산

--- src/test/java/basic/stack/PostfixTest.java
+++ src/test/java/basic/stack/PostfixTest.java
@@ -8,6 +8,16 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class PostfixTest {
 
+    /**
+     * 후위식 연산 결과 출력
+     * 후위식 연산 : 피연산자가 먼저 쓰이고, 그 뒤로 연산자가 나옴
+     * Stack 에 숫자이면 넣고 숫자가 아닐 경우(연산자) 2개의 숫자를 뽑는다.
+     * 먼저 뽑힌는 숫자는 right, 그다음 숫자는 left
+     * 연산자(+,-,*,/) 에 따라 연산을 진행하고 Stack 에 넣는다.
+     * 마지막 Stack 에 남아있는 숫자가 후위연산 결과가 됨
+     * -, / 연산 일 경우, left, right 가 중요함
+     */
+
     @Test
     void postfix() {
         String expression = "352+*9-";
@@ -27,20 +37,24 @@ class PostfixTest {
                     left = repository.pop();
                 }
             }
-            if (c == '+') {
-                repository.push(left + right);
-            }
-            if (c == '-') {
-                repository.push(left - right);
-            }
-            if (c == '*') {
-                repository.push(left * right);
-            }
-            if (c == '/') {
-                repository.push(left - right);
-            }
+            calculate(repository, c, left, right);
         }
         return repository.pop();
+    }
+
+    private static void calculate(Stack<Integer> repository, char c, int left, int right) {
+        if (c == '+') {
+            repository.push(left + right);
+        }
+        if (c == '-') {
+            repository.push(left - right);
+        }
+        if (c == '*') {
+            repository.push(left * right);
+        }
+        if (c == '/') {
+            repository.push(left - right);
+        }
     }
 
 }
