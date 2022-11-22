2d8392437e47047131113f3548368620a8afcd19
kanghyonghyun <hmoon826@gmail.com>
2022-11-22 19:09:23 +0900

    [refactor] Stack : 올바른 괄호인지 확인

--- src/test/java/basic/stack/BracketTest.java
+++ src/test/java/basic/stack/BracketTest.java
@@ -1,16 +1,23 @@
 package basic.stack;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-import java.util.Arrays;
-import java.util.EmptyStackException;
 import java.util.Stack;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class BracketTest {
+
+    /**
+     *  올바른 괄호인지 확인하는 문제
+     *  stack 의 특성을 이용 (후입선출)
+     *  stack 에 여는 괄호는 넣음
+     *  닫는 괄호 일 경우 stack 에 있는 여는 괄호를 뻄 (짝이 맞는 괄호)
+     *  만약, stack 이 비어 있다면 -> 닫는 괄호가 부족
+     *  모든 괄호를 살행하고, stack 이 비어있지 않다면 -> 여는 괄호가 부족 (올바른x)
+     *  stack 이 비어있으면 올바른 괄호 이다.
+     */
 
     @ParameterizedTest
     @CsvSource(
@@ -24,7 +31,6 @@ class BracketTest {
     }
 
     private String isProperBracket(String bracket) {
-        String result = "YES";
         Stack<Character> brackets = new Stack<>();
         for (char c : bracket.toCharArray()) {
             if (c == '(') {
@@ -37,9 +43,9 @@ class BracketTest {
             }
         }
         if (!brackets.isEmpty()) {
-            result = "NO";
+            return "NO";
         }
-        return result;
+        return "YES";
     }
 
 }
