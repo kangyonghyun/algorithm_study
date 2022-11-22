8006061bbe5504d05a4cc3cde03116442ba8bb3f
kanghyonghyun <hmoon826@gmail.com>
2022-11-22 16:31:46 +0900

    [refactor] HashMap : 두 단어가 Anagram 인지 판별하는 문제

--- src/test/java/basic/hashmap/AnagramTest.java
+++ src/test/java/basic/hashmap/AnagramTest.java
@@ -10,15 +10,19 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class AnagramTest {
 
-    // 알파벳 이 똑같다
-    // 알파뱃 갯수가 똑같다
+    /**
+     * 주어진 두 단어가 Anagram 인지 확인하는 문제
+     * 단어 한개를 map 에 저장
+     * 다른 단어를 같은 map 에 저장하면서 동일한 문자의 갯수를 뺴준다.
+     * 만약, 같은 단어가 없거나 (새로운 단어), 갯수가 0 이라면(많음) 아나그램이 아니다.
+     */
 
     @ParameterizedTest
     @CsvSource({
             "AbaAeCe, baeeACA",
             "zyzzY, Yzzzy"})
     void anagram_true(String word1, String word2) {
-        assertThat(solution(word1, word2)).isEqualTo("YES");
+        assertThat(isAnagram(word1, word2)).isEqualTo("YES");
     }
 
     @ParameterizedTest
@@ -27,22 +31,29 @@ class AnagramTest {
             "zzZyY, ZZzyy",
             "aaabb, aaabbB"})
     void anagram_false(String word1, String word2) {
-        assertThat(solution(word1, word2)).isEqualTo("NO");
+        assertThat(isAnagram(word1, word2)).isEqualTo("NO");
     }
 
-    private String solution(String word1, String word2) {
-        Map<Character, Integer> word1Map = new HashMap<>();
-        for (char c : word1.toCharArray()) {
-            word1Map.put(c, word1Map.getOrDefault(c, 0) + 1);
-        }
+    private String isAnagram(String word1, String word2) {
+        Map<Character, Integer> wordMap = new HashMap<>();
+        putWordToMap(word1, wordMap);
+        return getResult(word2, wordMap);
+    }
 
+    private static String getResult(String word2, Map<Character, Integer> wordMap) {
         for (char c : word2.toCharArray()) {
-            if (!word1Map.containsKey(c) || word1Map.get(c) == 0) {
+            if (!wordMap.containsKey(c) || wordMap.get(c) == 0) {
                 return "NO";
             }
-            word1Map.put(c, word1Map.get(c) - 1);
+            wordMap.put(c, wordMap.get(c) - 1);
         }
         return "YES";
+    }
+
+    private static void putWordToMap(String word1, Map<Character, Integer> wordMap) {
+        for (char c : word1.toCharArray()) {
+            wordMap.put(c, wordMap.getOrDefault(c, 0) + 1);
+        }
     }
 
 
