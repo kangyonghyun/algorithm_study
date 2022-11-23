7a51a9252082b21644ce3007178a0cbb14d38cfa
kanghyonghyun <hmoon826@gmail.com>
2022-11-23 18:08:23 +0900

    [refactor] BFS : 임의의 노드(토마토가 익은)로부터 최소 날짜 구하기

--- src/test/java/basic/bfs/RipeTomatoTest.java
+++ src/test/java/basic/bfs/RipeTomatoTest.java
@@ -9,6 +9,19 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class RipeTomatoTest {
 
+    /**
+     *  토마토가 모두 익을 때까지의 최소 날짜 출력
+     *  이미 모두 익었으면 0, 익지 못하는 상황 -1
+     *  익은 토마토들을 Que 에 넣음
+     *  Que 가 완전히 빌 때까지 빼고 넣고를 반복
+     *  Que 에서 토마토를 하나 빼고 토마토에 위치값을 더함
+     *  새로운 위치의 토마토가 조건을 만족하면
+     *    1. Que 에 저장
+     *    2. 주어진 2차원 배열에 익은 토마토(1) 표시
+     *    3. 날짜를 저장하는 2차원 배열에 날짜 +1
+     *  날짜를 저장하는 2차원 배열에서 최소 날짜를 구함
+     */
+
     @Test
     void solution() {
         int[][] box = new int[][]{
